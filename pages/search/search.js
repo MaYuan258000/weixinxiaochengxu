911d104df7252461bd256181a82b7c58799b8ce1
mayuan <m13717800979@163.com>
2018-03-01 16:58:26 +0800

    搜索

--- pages/search/search.js
+++ pages/search/search.js
@@ -7,18 +7,57 @@ Page({
    */
   data: {
     goods: goodsData,
-    historyList:['曹县','东关','西关','南关','北关','马远']
+    historyList:[],
+    inputValue:'',
+    searchinput :''
   },
   clearHistory(){
    this.setData({
      historyList:[]
    })
   },
+  //input事件来获取值
+  inputEvent(e){
+    var value=e.detail.value;
+    this.setData({
+      inputValue:value
+    })
+  },
+  //搜索存储数据
+  searchEvent(){
+    //获取input框的值
+    var input = this.data.inputValue;
+    //重新赋值 得到已经存储缓存中的数组
+    var array = wx.getStorageSync('history');
+    // if (array){
+    //   input && array.push(input);
+    // }else{
+    //   //如果不存在，我在push进去
+    //   array=[];
+    //   //判断input是不是存在用&&
+    
+    // }
+    //第二种方式
+    // if(!array){ 
+    //   array=[];
+    // }
+    //第三种方式 如果数组不存在，就给他一个新的数组[]
+    !array&&(array=[]);
+    //在input不为空的情况下进行push
+    input&&array.push(input);
+    wx.setStorageSync('history', array);
+    this.setData({
+      searchinput: '',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var list=wx.getStorageSync('history')
+    this.setData({
+      historyList:list
+    })
   },
 
   /**
